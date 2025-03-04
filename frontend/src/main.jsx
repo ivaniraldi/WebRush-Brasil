a5c365caf44774ef175a112e6bfa6ff01d27032b
ivaniraldi <iraldiban@gmail.com>
2025-03-04 11:24:43 -0300

    test changes bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,12 +8,17 @@ import { GlobalProvider } from "../contexts/GlobalContext";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <GlobalProvider>
-        <App />
-      </GlobalProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement && !rootElement.hasAttribute("data-react-root")) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <GlobalProvider>
+          <App />
+        </GlobalProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+  rootElement.setAttribute("data-react-root", "true"); // Marca como inicializado
+}
